6aab25d106eaa45759cf7a58438b51bd4e2635d7
weasley <1432656025@qq.com>
2023-07-16 23:13:37 +0800

    Use Base64 replace Base64Utils of Spring

--- mybatis-encrypt-spring-boot-starter/src/main/java/io/github/weasleyj/mybatis/encrypt/core/DefaultBase64EncryptStrategyImpl.java
+++ mybatis-encrypt-spring-boot-starter/src/main/java/io/github/weasleyj/mybatis/encrypt/core/DefaultBase64EncryptStrategyImpl.java
@@ -1,12 +1,12 @@
 package io.github.weasleyj.mybatis.encrypt.core;
 
 import io.github.weasleyj.mybatis.encrypt.annotation.EnableMybatisEncryption;
+import org.apache.commons.codec.binary.Base64;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.stereotype.Component;
 import org.springframework.util.Assert;
-import org.springframework.util.Base64Utils;
 
 import java.nio.charset.StandardCharsets;
 
@@ -24,12 +24,12 @@ public class DefaultBase64EncryptStrategyImpl implements EncryptStrategy {
     @Override
     public String encrypt(Object plaintext) {
         Assert.notNull(plaintext, "Plaintext cannot be null");
-        return Base64Utils.encodeToString(String.valueOf(plaintext).getBytes(StandardCharsets.UTF_8));
+        return Base64.encodeBase64String(String.valueOf(plaintext).getBytes(StandardCharsets.UTF_8));
     }
 
     @Override
     public String decrypt(Object ciphertext) {
         Assert.notNull(ciphertext, "Ciphertext cannot be null");
-        return new String(Base64Utils.decodeFromString(String.valueOf(ciphertext)), StandardCharsets.UTF_8);
+        return new String(Base64.decodeBase64(String.valueOf(ciphertext)), StandardCharsets.UTF_8);
     }
 }
--- mybatis-encrypt-spring-boot-starter/src/test/java/io/github/weasleyj/mybatis/encrypt/core/DefaultBase64EncryptStrategyImplTest.java
+++ mybatis-encrypt-spring-boot-starter/src/test/java/io/github/weasleyj/mybatis/encrypt/core/DefaultBase64EncryptStrategyImplTest.java
@@ -1,7 +1,12 @@
 package io.github.weasleyj.mybatis.encrypt.core;
 
+import org.apache.commons.codec.binary.Base64;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.util.Assert;
+import org.springframework.util.Base64Utils;
+
+import java.nio.charset.StandardCharsets;
 
 class DefaultBase64EncryptStrategyImplTest {
 
@@ -23,4 +28,34 @@ class DefaultBase64EncryptStrategyImplTest {
         Assert.isTrue(raw.equals(decrypt), "解密后的明文由于原始内容一致");
     }
 
+    public String encrypt(Object plaintext) {
+        Assert.notNull(plaintext, "Plaintext cannot be null");
+        return Base64Utils.encodeToString(String.valueOf(plaintext).getBytes(StandardCharsets.UTF_8));
+    }
+
+
+    public String decrypt(Object ciphertext) {
+        Assert.notNull(ciphertext, "Ciphertext cannot be null");
+        return new String(Base64Utils.decodeFromString(String.valueOf(ciphertext)), StandardCharsets.UTF_8);
+    }
+
+    @Test
+    @DisplayName("encoded1 == encoded2")
+    void func1() {
+        String encoded1 = encrypt(raw);
+        String encoded2 = Base64.encodeBase64String(raw.getBytes(StandardCharsets.UTF_8));
+        Assert.isTrue(encoded1.equals(encoded2), "encoded1 == encoded2");
+    }
+
+    @Test
+    @DisplayName("decoded1 == decoded2")
+    void func2() {
+        String encoded1 = encrypt(raw);
+        String encoded2 = Base64.encodeBase64String(raw.getBytes(StandardCharsets.UTF_8));
+        Assert.isTrue(encoded1.equals(encoded2), "encoded1 == encoded2");
+
+        String decrypt1 = decrypt(encoded1);
+        String decrypt2 = new String(Base64.decodeBase64(encoded2));
+        Assert.isTrue(decrypt1.equals(decrypt2), "decrypt1 == decrypt2");
+    }
 }
